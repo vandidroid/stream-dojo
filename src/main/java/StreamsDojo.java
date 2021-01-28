43438cdede8144ef287ec504b700f1b940ac2c87
Andi <1vandidroid1@gmail.com>
2021-01-28 09:19:51 +0100

    Init

---
+++ src/main/java/StreamsDojo.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamsDojo {
+    List<String> myList =
+            Arrays.asList("a1", "a2", "b1", "c2", "c1");
+
+    
+    public static void main(String[] args) {
+
+
+        System.out.println("sssssss");
+    }
+}
